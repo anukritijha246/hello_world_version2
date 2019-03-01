22dc16a48e7a72accf28350e8feba2c98bd28b83
anukriti <anukritijha246@gmail.com>
2019-03-01 23:12:06 +0530

    version 2

---
+++ models/Post.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    username: String,
+    companyid: {type:Number, default: -1},
+    upvote: {type:Number, default: 0 }
+});
+
+module.exports = mongoose.model("Post", PostSchema);
